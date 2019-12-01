d67d47b04cb259631e7d432257a4cb51bfdadac0
Kirill Kamakin <k.kamakin@protonmail.ch>
2019-12-01 22:52:45 +0300

    Show first feed from DB when index is requested
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- reader/views.py
+++ reader/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
+    feed = Feed.objects.first()
+    if feed:
+        feed.fetch()
+        posts = list(Post.objects.filter(feed=feed))
+    else:
+        posts = list()
+    return render(request, "index.html", locals())
